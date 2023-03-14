fce98f8ce7be611c7f641c8a294e21be74588614
김재형 <cogito1016@naver.com>
2023-03-14 19:00:55 +0900

    feat: socket server 설정

---
+++ socket_server/src/events/events.gateway.ts
@@ -0,0 +1,39 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway(3131, {
+  cors: { origin: '*' },
+})
+export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server = {} as Server;
+
+  users: number = 0;
+
+  //OnGatewayConnection를 오버라이딩
+  async handleConnection() {
+    this.users++; //사용자 증가
+    this.server.emit('users', this.users);
+    console.log(this.users);
+  }
+
+  //OnGatewayDisconnect를 오버라이딩
+  async handleDisconnect() {
+    this.users--; //사용자 감소
+    this.server.emit('users', this.users);
+    console.log(this.users);
+  }
+
+  @SubscribeMessage('chat')
+  async onChat(client: Socket, message: object) {
+    console.log(client.rooms); //현재 클라이언트의 방
+    console.log(message); //메시지
+    client.broadcast.emit('chat', message); //전체에게 방송함
+  }
+}
